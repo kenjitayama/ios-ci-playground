0b324a3b30e09454b0ed117bb5e5778a02fa587e
Kenji Tayama <kenji.tayama@gmail.com>
2019-08-27 12:22:10 +0900

    Initial Commit

---
+++ ios-ci-playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-ci-playground
+//
+//  Created by Kenji Tayama on 8/27/19.
+//  Copyright © 2019 Kenji Tayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
